2294a9f20ff5caf719382632fb7cb542aeb9a476
aebibtech <paul.camano@gmail.com>
2023-11-02 06:13:09 +0000

    Add user controller

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UsersController extends Controller
+{
+    public function loginForm(){
+
+    }
+
+    public function login(){
+        
+    }
+
+    public function registerForm(){
+
+    }
+
+    public function register(){
+
+    }
+
+    public function logout(){
+
+    }
+}
