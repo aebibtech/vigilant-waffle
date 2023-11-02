5e303dded5b13893c5d129eb48066e007d73191e
aebibtech <paul.camano@gmail.com>
2023-11-02 16:00:00 +0000

    Add button on recipe list page

--- resources/views/recipes/index.blade.php
+++ resources/views/recipes/index.blade.php
@@ -1,7 +1,10 @@
 @extends('layouts.recipes')
 @section('content')
     <section class="row g-2" id="recipes">
-        <h1 class="mb-5">Recipes</h1>
+        <h1 class="mb-5 d-flex justify-content-between">
+            <span>Recipes</span>
+            <a class="btn btn-success" href="{{ route('new.recipe') }}">Add New</a>
+        </h1>
     </section>
     <script src="{{ URL::asset('js/load_recipes.js') }}"></script>
 @endsection
